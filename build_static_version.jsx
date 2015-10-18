d07108c0f9ad1ae026a20b5a731cc273a66c2785
Jessica Claire Edwards <jessica@eml.cc>
2015-10-18 13:36:19 +1100

    Solved exercise 2 part 2

--- build_static_version.jsx
+++ build_static_version.jsx
@@ -3,7 +3,17 @@ import React from 'react';
 export const FilterableProductTable = React.createClass({
     render() {
 		    return (
-            <div></div>
+            <SearchBar/>
 		    )
+    }
+});
+
+export const SearchBar = React.createClass({
+    render() {
+        return (
+            <form>
+                <input type="search"/>
+            </form>
+        )
     }
 });
