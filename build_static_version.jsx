15f5afeb718f074490a33c62826e89e04024bfaa
Jessica Claire Edwards <jessica@eml.cc>
2015-10-18 14:06:00 +1100

    Solved exercise 2 part 3

--- build_static_version.jsx
+++ build_static_version.jsx
@@ -2,8 +2,12 @@ import React from 'react';
 
 export const FilterableProductTable = React.createClass({
     render() {
-		    return (
-            <SearchBar/>
+        const products = this.props.products;
+        return (
+            <div>
+                <SearchBar/>
+                <ProductTable products={products}/>
+            </div>
 		    )
     }
 });
@@ -14,6 +18,66 @@ export const SearchBar = React.createClass({
             <form>
                 <input type="search"/>
             </form>
+        )
+    }
+});
+
+export const ProductTable = React.createClass({
+    render() {
+        const products = this.props.products;
+        const rows = [];
+        let currentCategory;
+
+        products.forEach((product) => {
+            if (product.category !== currentCategory) {
+                currentCategory = product.category;
+
+                rows.push((
+                    <ProductCategoryRow
+                    key={currentCategory}
+                    category={currentCategory}
+                    />
+                ));
+            }
+            rows.push((
+                <ProductRow key={product.name} product={product.name}/>
+            ));
+        });
+        return (
+            <table>
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Price</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {rows}
+                </tbody>
+            </table>
+        )
+    }
+});
+
+export const ProductCategoryRow = React.createClass({
+    render() {
+        return (
+            <tr>
+                <th colSpan={2}>{this.props.category}</th>
+            </tr>
+        )
+    }
+});
+
+export const ProductRow = React.createClass({
+    render() {
+        
+        const product = this.props.product;
+        return (
+            <tr>
+                <td>{product.name}</td>
+                <td>{product.price}</td>
+            </tr>
         )
     }
 });
